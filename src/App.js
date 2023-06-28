8f494d4ef211f2558be72f3a2435d06e2b791d21
Ashok <cooperate.ashok@gmail.com>
2023-06-28 14:27:34 +0530

    updated

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import "./App.css";
+import { useState, useRef } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function App() {
+  const optionsArray = [
+    "India",
+    "United States",
+    "United Kingdom",
+    "japan",
+    "Canada",
+    "Germany",
+  ];
+  const [openselect, setOpenSelect] = useState(false);
+
+  const leagueInput = useRef();
+
+  function selectvalue(e) {
+    leagueInput.current.value = e.target.outerText;
+    setOpenSelect(false);
+  }
+
+  function openOption() {
+    setOpenSelect(true);
+  }
+  return (
+    <div className="App">
+      <div className="selectOption">
+        <input
+          onClick={openOption}
+          onBlur={() => {
+            setOpenSelect(false);
+          }}
+          ref={leagueInput}
+          id="league"
+          type="text"
+          placeholder="Select Your Country"
+          readOnly
+        />
+
+        <span className={openselect ? "icon active" : "icon"}>
+          <FontAwesomeIcon icon="fa-angle-down" />
+        </span>
+        
+        <div className={openselect ? "options active" : "options"}>
+          {optionsArray.map((item, index) => (
+            <li onClick={selectvalue} key={index}>
+              {item}
+            </li>
+          ))}
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
